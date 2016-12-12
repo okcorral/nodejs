8262a52c1e950a15ae96cffc8bd1dc57db42a631
Tom Corral <tom@botanyx.com>
2016-12-12 08:55:41 -0500

    express tutorial

--- customerapp/app.js
+++ customerapp/app.js
@@ -4,6 +4,43 @@ const path = require('path');
 
 const app = express();
 
-app.listen(3000, function(){
+/*
+const logger = function(req, res, next){
+	console.log('Logging...');
+	next();
+};
+
+app.use(logger);
+*/
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+const people = [
+	{
+		name:'Jeff',
+		age: 30
+	},
+	{
+		name:'Tom',
+		age: 51
+	},
+	{
+		name:'Sara',
+		age: 45
+	}
+]
+*/
+app.get('/', (req, res) => {
+	res.send('Hello World');
+	//res.json(people);
+});
+
+app.listen(3000, () => {
 	console.log('Server started on port 3000...');
-})
+});
